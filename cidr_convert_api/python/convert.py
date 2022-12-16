fd70dc3563b02e7e3c3d151dffb6a63e6bf67387
jonyemm <toojhonygomez@gmail.com>
2022-12-16 00:00:09 +0000

    coding

--- cidr_convert_api/python/convert.py
+++ cidr_convert_api/python/convert.py
@@ -2,7 +2,12 @@
 class CidrMaskConvert:
 
     def cidr_to_mask(self, val):
-        return val
+        val = int(val)
+        if 1 > val or val > 32:
+            return "Invalid"
+        val = '.'.join([str((0xffffffff << (32 - val) >> i) & 0xff)
+                    for i in [24, 16, 8, 0]])
+    return val
 
     def mask_to_cidr(self, val):
         return val
